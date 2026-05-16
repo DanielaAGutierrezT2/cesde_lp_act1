4960067c651db7ba4d08c14586f6d9fe5fcfdd27
DanielaAGutierrezT2 <dalexa-555@hotmail.com>
2026-05-16 18:20:39 +0000

    Avance Ejercicio 3

--- src/main/java/com/example/ConversionDeTipos.java
+++ src/main/java/com/example/ConversionDeTipos.java
@@ -3,13 +3,18 @@ package com.example;
 public class ConversionDeTipos {
     public static void demostrarCastingImplicitito() {
         // TODO
-    }
+    }   int variable1 = 5;
+        double variable2 = variable1;
 
     public static void demostrarCastingExplicito() {
         // TODO
+            double variable3 = 31.53;
+            int variable4 = (int) variable3; 
     }
 
     public static void demostrarProblemasDePrecision() {
         // TODO
+        long numeroGigante = 96325874154L;
+        short numeroCorto = (short) numeroGigante;
     }
 }
--- src/main/java/com/example/TiposDeDatosPrimitivos.java
+++ src/main/java/com/example/TiposDeDatosPrimitivos.java
@@ -3,18 +3,26 @@ package com.example;
 public class TiposDeDatosPrimitivos {
     public static void demostrarEnteros() {
         // TODO
+        byte dato1 = 115;
+        short dato2 = 25000;
+        int dato3 = 31;
+        long dato4 = 1;
     }
 
     public static void demostrarFlotantes() {
         // TODO
+        float precioCafe = 1.5F;
+        double precioCafeDolares = 3.5;
     }
 
     public static void demostrarCaracteres() {
         // TODO
+        char miInicial = 'p';
     }
 
     public static void demostrarBooleanos() {
         // TODO
+                boolean estadoLuz = true;
     }
 
     public static void demostrarValoresPorDefecto() {
