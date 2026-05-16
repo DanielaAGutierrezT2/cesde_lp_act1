d536c8c52f47c7550a9bb2ead5321ee5ca900bce
DanielaAGutierrezT2 <dalexa-555@hotmail.com>
2026-05-16 16:26:58 +0000

    Avance Ejercicio 1

--- src/main/java/com/example/VariablesYConstantes.java
+++ src/main/java/com/example/VariablesYConstantes.java
@@ -3,6 +3,11 @@ package com.example;
 public class VariablesYConstantes {
     public static void demostrarDeclaracionAsignacion() {        
         // TODO
+        int edad=20;
+        double estatura = 1.8;
+        System.out.println(edad);
+        System.out.println(estatura);
+
     }
 
     public static void demostrarConvencionesNombres() {
